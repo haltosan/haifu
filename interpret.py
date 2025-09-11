0ed9770e721d158fe808cd705697b9d9b106ee04
haltosan <32181538+haltosan@users.noreply.github.com>
2025-09-11 17:33:20 -0600

    Interpreted math-simplified

--- interpret.py
+++ interpret.py
@@ -1,6 +1,62 @@
+from math import ceil, floor
+
 data = dict()
-#TODO get element relationship table
-element_relationship = dict()
+
+YIN = 2
+YANG = 1
+
+def strive(x):
+    #TODO incomplete
+    if x > 0:
+        return ceil(x)
+    return floor(x)
+
+def yin_or_yang(x) -> int:
+    if type(x) is float:
+        x = strive(x)
+    return YIN if x % 2 == 0 else YANG
+
+def element_relationship(element_a, element_b=None, relationship_type=None):
+    create_relationship = {
+        'earth': 'metal',
+        'metal': 'water',
+        'water': 'wood',
+        'wood': 'fire',
+        'fire': 'earth'
+    }
+    destroy_relationship = {
+        'earth': 'water',
+        'water': 'fire',
+        'fire': 'metal',
+        'metal': 'wood',
+        'wood': 'earth'
+    }
+    love_relationship = {
+        'earth': 'fire',
+        'fire': 'wood',
+        'wood': 'water',
+        'water': 'metal',
+        'metal': 'earth'
+    }
+    fear_relationship = {
+        'earth': 'wood',
+        'wood': 'metal',
+        'metal': 'fire',
+        'fire': 'water',
+        'water': 'earth'
+    }
+    relationship = {
+        'create':create_relationship,
+        'destroy':destroy_relationship,
+        'love':love_relationship,
+        'fear':fear_relationship
+    }
+
+    a_b_relationship = {(a,relationship[t][a]):t for t in relationship for a in relationship[t]}
+
+    if element_b is None:
+        return relationship[relationship_type][element_a]
+    return a_b_relationship[element_a, element_b]
 
 def run(bureaucracy, debug=False):
     """expect program of form [lowest, ... highest]"""
@@ -17,13 +73,28 @@ def run(bureaucracy, debug=False):
         if rung in ['heaven', 'nirvana', 'enlightenment', 'harmony']:
             dprint('halt')
             return
+
         elif rung in ['rise', 'float', 'ascend', 'up']:
             dprint('up')
-            delegate += 1
+            lower = bureaucracy[bureaucrat - 1]
+            if type(lower) is int:
+                delegate += lower
+            elif ' ' in lower:
+                var_name = lower.split(' ')[0]
+                var_data = data[var_name][1]
+                if type(var_data) is int:
+                    delegate += var_data
+            else:
+                delegate += 1
+
         elif rung in ['count', 'number', 'age']:
             dprint('number')
-            if type(bureaucracy[delegate]) is int:
+            drung = bureaucracy[delegate]
+            if type(drung) is int:
                 print(bureaucracy[delegate])
+            elif ' ' in drung:
+                print(data[drung.split(' ')[0]][1])
+
         elif rung == '/':
             dprint('punc')
             data_tmp = []
@@ -33,54 +104,67 @@ def run(bureaucracy, debug=False):
             while (rung := bureaucracy[bureaucrat]) not in ['/', var_name]:
                 data_tmp.append(rung)
                 bureaucrat += 1
-            bureaucrat += 1
             if len(data_tmp) == 1:
                 data_tmp = data_tmp[0]
             data[var_name] = [var_type, data_tmp]
+
         elif rung in ['operate', 'examine', 'study']:
+            dprint('operate')
             b = bureaucracy[delegate]
             a = bureaucracy[delegate + 1]
+            result = op(a, b)
+            b_name = b.split(' ')[0]
+            data[b_name][1] = result
+
+        elif type(rung) is int:
+            dprint('int literal')
+            pass
 
         elif ' ' in rung:  # variable
             dprint('var literal')
             #TODO implement
             pass
-        elif type(rung) is int:
-            dprint('int literal')
-            pass
+
         else:
             dprint('other')
             pass
+
         bureaucrat += 1
 
 def op(a, b):
     global data
-    a_name, a_type = a.split(' ')
-    b_name, b_type = b.split(' ')
-    match element_relationship[b_type, a_type]:
-        case 'creates':
-            '+'
-        case 'destroys':
-            '-'
-        case 'fears':
-            '/'
-        case 'loves':
-            '*'
+    a_name = a.split(' ')[0]
+    b_name = b.split(' ')[0]
+    a_type, a_val = data[a_name]
+    if not isinstance(a_val, (int, float)):
+        return None
+    b_type, b_val = data[b_name]
+    if not isinstance(b_val, (int, float)):
+        return None
+    match element_relationship(b_type, a_type):
+        case 'create':
+            return a_val + b_val
+        case 'destroy':
+            return a_val - b_val
+        case 'fear':
+            return a_val / b_val
+        case 'love':
+            return a_val * b_val
         case _:
-            'yin/yang'
-        #TODO finish
+            if yin_or_yang(a_val) == YANG and yin_or_yang(b_val) == YANG:
+                return YANG
+            return YIN
 
 if __name__ == '__main__':
     just_exit = ['nirvana']
     print_123 = [1, 2, 3, 'number', 'up', 'number', 'up', 'number', 'heaven']
-    math = ['/', 'v1 earth', 1, '/', '/', 'v2 metal', 2, '/', '/', 'v3 wood', 3, '/', '/', 'v4 metal', 4, '/', '/', 'v5 earth', '/'
+    math = ['/', 'v1 earth', 1, '/', '/', 'v2 metal', 2, '/', '/', 'v3 wood', 3, '/', '/', 'v4 metal', 4, '/', '/', 'v5 earth', 5, '/',
             'v1 earth', 'v2 metal', 'v3 wood', 'v4 metal', 'v5 earth',
-            23,
+            20,
             'up', 'study', 'age',  # v1
             'up', 'study', 'age',  # v2
             'up', 'study', 'age',  # v3
             'up', 'study', 'age',  # v4
-            'up', 'study', 'age',  # v5
             'nirvana']
     #programs = [just_exit, print_123, math]
     programs = [math]
