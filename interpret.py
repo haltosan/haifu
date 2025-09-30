3fa65bfadd0ad9fce6c091f412c83fb9a5d3e9b9
haltosan <32181538+haltosan@users.noreply.github.com>
2025-09-30 11:08:34 -0600

    Change token type from string to enum

--- interpret.py
+++ interpret.py
@@ -1,11 +1,46 @@
 from math import ceil, floor
+from enum import Enum
 #TODO use enums for tokens instead of strings
+# double check the changes actually did something
 
 data = dict()
 #TODO make structs for variables instead of strings
 
 YIN = 2
 YANG = 1
+
+
+class TokenType(Enum):
+    HEAVEN = 0
+    PROMOTE = 1
+    DEMOTE = 2
+    BLOSSOM = 3
+    RISE = 4
+    FALL = 5
+    LISTEN = 6
+    SPEAK = 7
+    COUNT = 8
+    CREATE = 9
+    DESTROY = 10
+    FEAR = 11
+    LOVE = 12
+    BECOME = 13
+    LIKE = 14
+    NEGATIVE = 15
+    OPERATE = 16
+    PUNC = 17
+    INT = 18
+    VAR = 19
+
+
+class Token:
+    t:TokenType = None
+    value = None
+
+    def __init__(self, t, value=None):
+        self.t = t
+        self.value = value
+
 
 def strive(x):
     #TODO incomplete
@@ -71,109 +106,122 @@ def run(bureaucracy, debug=False):
             print('\t', txt)
 
     while bureaucrat < len(bureaucracy):
-        rung = bureaucracy[bureaucrat]
-        if rung in ['heaven', 'nirvana', 'enlightenment', 'harmony']:
-            dprint('halt')
-            return
-
-        elif rung in ['rise', 'float', 'ascend', 'up',
-                      'fall', 'drop', 'descend', 'down']:
-            if rung in ['fall', 'drop', 'descend', 'down']:
-                dprint('fall')
-                sign = -1
-            else:
-                dprint('up')
-                sign = 1
-
-            lower = bureaucracy[bureaucrat - 1]
-            if type(lower) is int:
-                delegate += lower * sign
-            elif ' ' in lower:
-                var_name = lower.split(' ')[0]
-                var_data = data[var_name][1]
-                if type(var_data) is int:
-                    delegate += var_data * sign
-            else:
-                delegate += 1 * sign
-
-            if delegate < 0:
-                delegate = 0
-            elif delegate > bureaucrat:
-                delegate = bureaucrat
-
-        elif rung in ['demote', 'less', 'reduce', 'wane']:
-            dprint('demote')
-            d_rung = bureaucracy[delegate]
-            if type(d_rung) is int:
-                diff = d_rung
-            elif ' ' in d_rung:
-                var_name = d_rung.split(' ')[0]
-                var_data = data[var_name][1]
-                if type(var_data) is int:
-                    diff = var_data
-                else:
-                    diff = 0
-            else:
-                diff = 0
-            bureaucrat -= diff
-
-            if bureaucrat < 0:
-                bureaucrat = 0
-            elif bureaucrat >= len(bureaucracy):
+        rung:Token = bureaucracy[bureaucrat]
+        match rung.t:
+            case TokenType.HEAVEN:
+                dprint('halt')
                 return
+            case TokenType.RISE | TokenType.FALL:
+                dprint('rise/fall')
+                if rung.t == TokenType.FALL:
+                    sign = -1
+                else:
+                    sign = 1
 
-            if delegate > bureaucrat:
-                delegate = bureaucrat
+                lower:Token = bureaucracy[bureaucrat - 1]
+                match lower.t:
+                    case TokenType.INT:
+                        delegate += lower.value * sign
+                    case TokenType.VAR:
+                        #TODO implement
+                        pass
+                    case _:
+                        delegate += 1 * sign
+                # elif ' ' in lower:
+                #     var_name = lower.split(' ')[0]
+                #     var_data = data[var_name][1]
+                #     if type(var_data) is int:
+                #         delegate += var_data * sign
 
-        elif rung in ['count', 'number', 'age']:
-            dprint('number')
-            d_rung = bureaucracy[delegate]
-            if type(d_rung) is int:
-                print(d_rung, end='')
-            elif ' ' in d_rung:
-                print(data[d_rung.split(' ')[0]][1], end='')
+                if delegate < 0:  # TODO move clamping to end of loop
+                    delegate = 0
+                elif delegate > bureaucrat:
+                    delegate = bureaucrat
+                continue
 
-        elif rung in ['speak', 'say', 'draw']:
-            dprint('say')
-            d_rung = bureaucracy[delegate]
-            if type(d_rung) is int:
-                print(chr(d_rung), end='')
-            elif ' ' in d_rung:
-                print(chr(data[d_rung.split(' ')[0][1]]), end='')
+            case TokenType.PROMOTE | TokenType.DEMOTE:
+                dprint('promote/demote')
+                if rung.t == TokenType.DEMOTE:
+                    sign = -1
+                else:
+                    sign = 1
+                d_rung:Token = bureaucracy[delegate]
+                diff = 0
+                match d_rung.t:
+                    case TokenType.INT:
+                        diff = d_rung
+                    case TokenType.VAR:
+                        #TODO implement
+                        diff = 0
+                        # var_name = d_rung.split(' ')[0]
+                        # var_data = data[var_name][1]
+                        # if type(var_data) is int:
+                        #     diff = var_data
+                        # else:
+                        #     diff = 0
+                    case _:
+                        diff = 0
+                bureaucrat += diff * sign  # TODO make consistent
 
-        elif rung == '/':
-            dprint('punc')
-            data_tmp = []
-            bureaucrat += 1
-            var_name, var_type = bureaucracy[bureaucrat].split(' ')
-            bureaucrat += 1
-            while (rung := bureaucracy[bureaucrat]) not in ['/', var_name]:
-                data_tmp.append(rung)
+                if bureaucrat < 0:
+                    bureaucrat = 0
+                elif bureaucrat >= len(bureaucracy):
+                    return
+
+                if delegate > bureaucrat:
+                    delegate = bureaucrat
+
+            case TokenType.COUNT | TokenType.SPEAK:
+                dprint('print')
+                if rung.t == TokenType.SPEAK:
+                    def cast(x):
+                        return chr(x)
+                else:
+                    def cast(x):
+                        return x
+
+                d_rung:Token = bureaucracy[delegate]
+                match d_rung.t:
+                    case TokenType.INT:
+                        print(cast(d_rung.value), end='')
+                    case TokenType.VAR:
+                        # TODO implement
+                        pass
+                # elif ' ' in d_rung:
+                #     print(data[d_rung.split(' ')[0]][1], end='')
+
+            case TokenType.PUNC:
+                dprint('punc')
+                data_tmp = []
                 bureaucrat += 1
-            if len(data_tmp) == 1:
-                data_tmp = data_tmp[0]
-            data[var_name] = [var_type, data_tmp]
+                # TODO redo with new var structure
+                var_name, var_type = bureaucracy[bureaucrat].split(' ')
+                bureaucrat += 1
+                while (rung := bureaucracy[bureaucrat]) not in ['/', var_name]:
+                    data_tmp.append(rung)
+                    bureaucrat += 1
+                if len(data_tmp) == 1:
+                    data_tmp = data_tmp[0]
+                data[var_name] = [var_type, data_tmp]
 
-        elif rung in ['operate', 'examine', 'study']:
-            dprint('operate')
-            b = bureaucracy[delegate]
-            a = bureaucracy[delegate + 1]
-            result = op(a, b)
-            b_name = b.split(' ')[0]
-            data[b_name][1] = result
+            case TokenType.OPERATE:
+                dprint('operate')
+                b = bureaucracy[delegate]
+                a = bureaucracy[delegate + 1]
+                result = op(a, b)
+                #TODO new var structure
+                b_name = b.split(' ')[0]
+                data[b_name][1] = result
 
-        elif type(rung) is int:
-            dprint('int literal')
-            pass
+            case TokenType.INT:
+                dprint('int literal')
 
-        elif ' ' in rung:  # variable
-            dprint('var literal')
-            #TODO implement
-            pass
+            case TokenType.VAR:
+                dprint('var literal')
+                #TODO implement
 
-        else:
-            dprint('other')
-            pass
+            case _:
+                dprint("other")
 
         bureaucrat += 1
 
@@ -202,36 +250,52 @@ def op(a, b):
             return YIN
 
 if __name__ == '__main__':
-    just_exit = ['nirvana']
-    print_123 = [1, 2, 3, 'number', 'up', 'number', 'up', 'number', 'heaven']
-    math = ['/', 'v1 earth', 1, '/', '/', 'v2 metal', 2, '/', '/', 'v3 wood', 3, '/', '/', 'v4 metal', 4, '/', '/', 'v5 earth', 5, '/',
-            'v1 earth', 'v2 metal', 'v3 wood', 'v4 metal', 'v5 earth',
-            20,
-            'up', 'study', 'age',  # v1
-            'up', 'study', 'age',  # v2
-            'up', 'study', 'age',  # v3
-            'up', 'study', 'age',  # v4
-            'nirvana']
-    hello_world = [104, 101, 108, 111, 32,
-                   119, 111, 114, 108, 100, 10,
-                   'say', 'up',  # h
-                   'say', 'up',  # e
-                   'say', 'say', 'up',  # ll
-                   'say', 'up',  # o
-                   'say', 'up',
-                   'say', 'up',  # w
-                   'say', 'up',  # o
-                   'say', 'up',  # r
-                   'say', 'up',  # l
-                   'say', 'up',  # d
-                   'say', 'up',  # \n
+    just_exit = [Token(TokenType.HEAVEN)]
+    print_123 = [Token(TokenType.INT, 1), Token(TokenType.INT, 2), Token(TokenType.INT, 3),
+                 Token(TokenType.COUNT), Token(TokenType.RISE),
+                 Token(TokenType.COUNT), Token(TokenType.RISE),
+                 Token(TokenType.COUNT), Token(TokenType.HEAVEN)]
+    math = [Token(TokenType.PUNC), Token(TokenType.VAR, 'v1 earth'),
+            Token(TokenType.INT, 1), Token(TokenType.PUNC),
+            Token(TokenType.PUNC), Token(TokenType.VAR, 'v2 metal'),
+            Token(TokenType.INT, 2), Token(TokenType.PUNC),
+            Token(TokenType.PUNC), Token(TokenType.VAR, 'v3 wood'),
+            Token(TokenType.INT, 3), Token(TokenType.PUNC),
+            Token(TokenType.PUNC), Token(TokenType.VAR, 'v4 metal'),
+            Token(TokenType.INT, 4), Token(TokenType.PUNC),
+            Token(TokenType.PUNC), Token(TokenType.VAR, 'v5 earth'),
+            Token(TokenType.INT, 5), Token(TokenType.PUNC),
+            Token(TokenType.VAR, 'v1 earth'), Token(TokenType.VAR, 'v2 metal'),
+            Token(TokenType.VAR, 'v3 wood'), Token(TokenType.VAR, 'v4 metal'),
+            Token(TokenType.VAR, 'v5 earth'),
+            Token(TokenType.INT, 20),
+            Token(TokenType.RISE), Token(TokenType.OPERATE), Token(TokenType.COUNT),  # v1
+            Token(TokenType.RISE), Token(TokenType.OPERATE), Token(TokenType.COUNT),  # v2
+            Token(TokenType.RISE), Token(TokenType.OPERATE), Token(TokenType.COUNT),  # v3
+            Token(TokenType.RISE), Token(TokenType.OPERATE), Token(TokenType.COUNT),  # v4
+            Token(TokenType.HEAVEN)]
+    hello_world = [Token(TokenType.INT, 104), Token(TokenType.INT, 101), Token(TokenType.INT, 108),
+                   Token(TokenType.INT, 111), Token(TokenType.INT, 32),
+                   Token(TokenType.INT, 119), Token(TokenType.INT, 111), Token(TokenType.INT, 114),
+                   Token(TokenType.INT, 108), Token(TokenType.INT, 100), Token(TokenType.INT, 10),
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # h
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # e
+                   Token(TokenType.SPEAK), Token(TokenType.RISE), Token(TokenType.RISE),  # ll
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # o
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # w
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # o
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # r
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # l
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # d
+                   Token(TokenType.SPEAK), Token(TokenType.RISE),  # \n
                    ]
-    loop = [1,  # print val
-            4,  # loop jump back
-            'fall',
-            'count',
-            'rise',
-            'demote']
+    loop = [Token(TokenType.INT, 1),  # print val
+            Token(TokenType.INT, 4),  # loop jump back
+            Token(TokenType.FALL),
+            Token(TokenType.COUNT),
+            Token(TokenType.RISE),
+            Token(TokenType.DEMOTE)]
     #programs = [just_exit, print_123, math]
     programs = [loop]
 
